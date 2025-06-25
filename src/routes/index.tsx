0c6306ac904a9d0b6895856a8ee9235103499086
Chph21 <110610320+Chph21@users.noreply.github.com>
2025-06-25 15:40:53 +0700

    Updated index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
-import Layout from '../components/layout/Layout';
+import Layout from '../components/Layout/Layout';
 import ChatBotPage from '../pages/ChatBot/ChatBotPage';
 
 const router = createBrowserRouter([
